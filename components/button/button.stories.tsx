8ef6823f5085e0d7b50de47df9a3c0b1b9e02c22
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2022-11-11 13:36:05 +0100

    Update button.stories.tsx

--- components/button/button.stories.tsx
+++ components/button/button.stories.tsx
@@ -5,9 +5,6 @@ import { Button } from "./button";
 export default {
     title: "Components/Button",
     component: Button,
-    argTypes: {
-        disabled: { control: "boolean" },
-    },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
