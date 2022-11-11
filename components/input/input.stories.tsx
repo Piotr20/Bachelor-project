05eda95f022b4e63402d8728f84dd06255eba24e
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2022-11-11 13:33:51 +0100

    input story

--- components/input/input.stories.tsx
+++ components/input/input.stories.tsx
@@ -3,32 +3,14 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Input } from "./input";
 
 export default {
-  title: "Components/Input",
-  component: Input,
-  argTypes: {
-    disabled: { control: "boolean" },
-    disableMaxInputLenght: { control: "boolean" },
-    searchIconLeft: { control: "boolean" },
-    searchIconRight: { control: "boolean" },
-    hideTextMessage: { control: "boolean" },
-  },
+    title: "Components/Input",
+    component: Input,
 } as ComponentMeta<typeof Input>;
 
 const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  color: "default",
-  label: "Input",
-  textMessage: "Text message",
-  placeholderInputText: "Input text",
-  maxInputLenght: 300,
-  disableMaxInputLenght: true,
-  textAlignment: "start",
-  hideTextMessage: true,
-  disabled: false,
-  inputIconLeft: false,
-  searchIconRight: false,
-  inputId: "",
-  inputType: "",
+    placeholder: "default value",
+    type: "text",
 };
